3e8b0e8ee6f565eab1aebb0b9b3229eec2b506e0
Dilker15 <68875465+Dilker15@users.noreply.github.com>
2021-08-30 23:16:53 -0400

    Add files via upload

---
+++ ProyectoGrafos/src/proyectografos/ExceptionesNroVerticeInvalidos.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectografos;
+/**
+ *
+ * @author hp
+ */
+public class ExceptionesNroVerticeInvalidos extends Exception {
+
+    /**
+     * Creates a new instance of <code>ExceptionesDeGrafos</code> without detail
+     * message.
+     */
+    public ExceptionesNroVerticeInvalidos() {
+        super("el numero de vertices es invalido");
+    }
+
+    /**
+     * Constructs an instance of <code>ExceptionesDeGrafos</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public ExceptionesNroVerticeInvalidos(String msg) {
+        super(msg);
+    }
+}
